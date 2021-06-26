ea073802a563ef8f553664199ad79e5f9fd2e66c
hanchettbm <hanchettbm@gmail.com>
2021-06-26 17:25:00 -0600

    Pong First Commit

--- Pong/GameScene.swift
+++ Pong/GameScene.swift
@@ -10,79 +10,118 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var ball = SKSpriteNode()
+    var player1 = SKSpriteNode()
+    var player2 = SKSpriteNode()
+    
+    var topLabel = SKLabelNode()
+    var bottomLabel = SKLabelNode()
+    
+    var score = [Int]()
     
     override func didMove(to view: SKView) {
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        startGame()
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        topLabel = self.childNode(withName: "topLabel")! as! SKLabelNode
+        bottomLabel = self.childNode(withName: "bottomLabel")! as! SKLabelNode
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
+        ball = self.childNode(withName: "ball")! as! SKSpriteNode
+        player1 = self.childNode(withName: "player1")! as! SKSpriteNode
+        player2 = self.childNode(withName: "player2")! as! SKSpriteNode
+        
+        player2.position.y = (self.frame.height / 2) - 30
+        player1.position.y = (-self.frame.height / 2) + 30
+        var randomInt = Int.random(in: -15..<15)
+        while randomInt == 0 {
+            randomInt = Int.random(in: -15..<15)
         }
+        ball.physicsBody?.applyImpulse(CGVector(dx: randomInt, dy: -15))
+        
+        let border = SKPhysicsBody(edgeLoopFrom: self.frame)
+        
+        border.friction = 0
+        border.restitution = 1
+        
+        self.physicsBody = border
+        
     }
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+    func startGame() {
+        score = [0,0]
+        topLabel.text = "\(score[1])"
+        bottomLabel.text = "\(score[0])"
     }
     
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
+    func addScore(playerPoint : SKSpriteNode){
+        
+        ball.position = CGPoint(x: 0, y: 0)
+        ball.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
+        
+        if playerPoint == player1 {
+            score[0] += 1
+            var randomInt = Int.random(in: -15..<15)
+            while randomInt == 0 {
+                randomInt = Int.random(in: -15..<15)
+            }
+            ball.physicsBody?.applyImpulse(CGVector(dx: randomInt, dy: 15))
         }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
+        else if playerPoint == player2 {
+            score[1] += 1
+            var randomInt = Int.random(in: -15..<15)
+            while randomInt == 0 {
+                randomInt = Int.random(in: -15..<15)
+            }
+            ball.physicsBody?.applyImpulse(CGVector(dx: randomInt, dy: -15))
         }
+        topLabel.text = "\(score[1])"
+        bottomLabel.text = "\(score[0])"
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        for touch in touches {
+            
+            let location = touch.location(in: self)
+            
+            if location.y > 0 {
+                player2.run(SKAction.moveTo(x: location.x, duration: 0.1))
+            }
+            if location.y < 0{
+                player1.run(SKAction.moveTo(x: location.x, duration: 0.1))
+            }
         }
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+        for touch in touches {
+            
+            let location = touch.location(in: self)
+            
+            if location.y > 0 {
+                player2.run(SKAction.moveTo(x: location.x, duration: 0.1))
+            }
+            if location.y < 0{
+                player1.run(SKAction.moveTo(x: location.x, duration: 0.1))
+            }
+            
+        }
     }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+        
+//        player2.run(SKAction.moveTo(x: ball.position.x, duration: 1.0))
+        
+        if ball.position.y <= player1.position.y - 18 {
+            addScore(playerPoint: player2)
+            
+        }
+        else if ball.position.y >= player2.position.y + 18 {
+            addScore(playerPoint: player1)
+
+            
+        }
     }
 }
